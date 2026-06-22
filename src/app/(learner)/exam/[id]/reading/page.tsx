a8ff7b121a7c04582800e9d6ad6b2f239e18fc48
ngdi2006 <141154561+ngdi2006@users.noreply.github.com>
2026-06-22 12:31:03 +0700

    updawte

--- src/app/(learner)/exam/[id]/reading/page.tsx
+++ src/app/(learner)/exam/[id]/reading/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useCallback } from 'react'
 import { useParams, useRouter, useSearchParams } from 'next/navigation'
 import { Button } from '@/components/ui/button'
 import { Card } from '@/components/ui/card'
